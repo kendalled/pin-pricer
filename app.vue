9b7292f02697e6cca052be458eaeff2432f85363
Kendall Jackson <kendallkj00@gmail.com>
2025-07-15 16:27:27 -0400

    feat: initial commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <h1 class="text-3xl font-bold underline text-blue-600">
+      Tailwind CSS 4 is working!
+    </h1>
+    <NuxtWelcome />
+  </div>
+</template>
